f1b73ba8306a1620ac4e3ed05c192bdfd613147b
vinay_sigmato <vinay.sigmato@gmail.com>
2024-11-19 18:08:43 +0530

    "Refactor GitHub Actions workflow for deploying Node.js app to Azure Web App and update server.js to use uppercase PORT variable"

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello from the backend using Express!" });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
