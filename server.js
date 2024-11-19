272ce7fa1ce274885af877225ab6f1ff6ccacaad
vinay_sigmato <vinay.sigmato@gmail.com>
2024-11-19 17:56:08 +0530

    "Updated package.json script formatting and server.js port configuration"

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.get("/api/hello", (req, res) => {
@@ -12,4 +12,3 @@ app.get("/api/hello", (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-
